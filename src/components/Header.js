9851970737079910df605d7a49627f4a74188bc6
BotanicalCoder <57745557+BotanicalCoder@users.noreply.github.com>
2020-11-27 21:24:31 +0100

    update header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,12 @@ import React from "react";
 import styled from "styled-components";
 
 import Carousel from "./Carousel";
-import img from "../images/1014891-cool-morbid-wallpaper-1920x1200.jpg";
 
 const Header = () => {
   return (
     <Mainheader>
       <div>
-        <img src={img} alt="" />
+       
         <div><Carousel/></div>
       </div>
     </Mainheader>
@@ -17,13 +16,9 @@ const Header = () => {
 };
 
 const Mainheader = styled.header`
-margin-right:-2em;
+height:70vh;
 
-  img {
-    height: 70vh;
-    width: 100%;
-    margin-right:-2em;
-  }
+ 
 `;
 
 export default Header;
